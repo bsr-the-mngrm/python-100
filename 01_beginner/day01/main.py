e8cddf76113ba94e2f4d651f12634cc00ccb9506
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-11-20 19:03:52 +0100

    Day01 solution

--- 01_beginner/day01/main.py
+++ 01_beginner/day01/main.py
@@ -1,1 +1,11 @@
-input("What is your name? ")
+# DAY ONE - SOLUTION
+
+# Welcome message
+print ("#DAY01 - Welcome in my \"Band name generator\"! \n\n")
+
+# Get the inputs - city: str; pet: str
+city = input("What's name of the city you grew up in? ")
+pet = input("What's name of your pet? ")
+
+# Give back the Band name which is the value of the city plus the pet variables
+print ("\nThe Band name is: " + city + " " + pet)
