a6345c300b68d29a171e2dc6565200cdaaf6513a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-11-22 22:14:12 +0100

    Update day03_project.py

---
+++ 01_beginner/day03/day03_project.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+# First Choice
+choice = input("You arrived at a Cross Road. Which way do you choose? Left or right? ")
+
+if choice.lower() == "left":
+    # Second Choice
+    choice = input("Great choice. Your road ended on a river coast. Do you want to swim or wait? ")
+    if choice.lower() == "wait":
+        # Third Choice
+        choice = input("Suddenly three magic doors appear. Which door do you choose? The red, the yellow or the blue one? ")
+        if choice.lower() == "yellow":
+            print("You found the Treasure Island. You Win!")
+        elif choice.lower() == "red":
+            print("No way! This is the Hell. Game over.")
+        elif choice.lower() == "blue":
+            print("Back to home. You've forgotten everything that just happened on the last day. Game over.")
+        else: 
+            print("Game Over.")
+    else:
+        print("Game Over.")        
+else:
+    print("Game Over.")
