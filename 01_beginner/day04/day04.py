a65ddea43d80aa26832fc2cc885c243ae04824fc
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-11-23 22:57:34 +0100

    Update day04.py

--- 01_beginner/day04/day04.py
+++ 01_beginner/day04/day04.py
@@ -1,14 +1,27 @@
 import random
 import my_module
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
 
-print(my_module.pi)
+# print(my_module.pi)
 
-random_float = random.random()
-print(random_float)
+# random_float = random.random()
+# print(random_float)
 
-love_score = random.randint(1,100)
-print(f"Your love score is {love_score}")
+# love_score = random.randint(1,100)
+# print(f"Your love score is {love_score}")
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", \
+                    "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", \
+                    "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", \
+                    "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", \
+                    "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", \
+                    "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", \
+                    "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", \
+                    "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(len(states_of_america))
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
 
