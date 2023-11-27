17c8a228b90fd76bbeffff78f652e3443443c511
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-11-27 13:03:21 +0100

    day08 - caesar cipher part 4 - final

--- 01_beginner/day08/day08.py
+++ 01_beginner/day08/day08.py
@@ -1,22 +1,39 @@
+from os import system
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-def caesar(cipher_direction, start_text, shift_amount):
-    end_text = ""
-
-    if cipher_direction == "decode":
-            shift_amount *= -1
-    
-    for letter in start_text:
-        position = alphabet.index(letter)
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+      end_text += char 
+    else:
+        position = alphabet.index(char)
         new_position = position + shift_amount
         end_text += alphabet[new_position]
-    
-    print(f"The {cipher_direction}d text is {end_text}")
         
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-caesar(cipher_direction=direction, start_text=text, shift_amount=shift)
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+wants_to_continue = True
+
+while wants_to_continue:
+    print (logo)
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+
+    system('cls')
+    
+    if result == "no":
+       wants_to_continue = False
+       print("Goodbye")
