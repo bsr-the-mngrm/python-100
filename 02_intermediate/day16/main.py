e3d3eb3fdf2b3003c15d9bbd9c09293108b942c1
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-04 22:51:30 +0100

    Update main.py

--- 02_intermediate/day16/main.py
+++ 02_intermediate/day16/main.py
@@ -1,3 +1,13 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+if __name__ == '__main__':
+    my_coffee_maker = CoffeeMaker()
+    my_menu = Menu()
+    my_money_machine = MoneyMachine()
+
+    user_input = ""
+
+    while user_input != "off":
+        user_input = input()
