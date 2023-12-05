eef795e39387890ca1effdd71cb512746595e82b
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-05 19:53:50 +0100

    Update quiz_brain.py

--- 02_intermediate/day17/quiz_brain.py
+++ 02_intermediate/day17/quiz_brain.py
@@ -1,5 +1,5 @@
 class QuizBrain:
 
-    def __init__(self, question_list):
+    def __init__(self, q_list):
         self.question_number = 0
-        self.question_list = question_list
+        self.question_list = q_list
