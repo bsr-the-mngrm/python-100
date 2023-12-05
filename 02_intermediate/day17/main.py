71f9013ba49e20048be021a7d9e2ea122a92d8d4
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-05 19:52:46 +0100

    Update main.py

--- 02_intermediate/day17/main.py
+++ 02_intermediate/day17/main.py
@@ -1,3 +1,10 @@
+from data import question_data
+from question_model import Question
 
 if __name__ == '__main__':
-    pass
+    question_bank = []
+
+    for question in question_data:
+        question_bank.append(Question(question['text'], question['answer']))
+
+
