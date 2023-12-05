67bca7db70484e56aaf12ea0c93c47e439e9bfb2
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-05 20:38:39 +0100

    Update main.py

--- 02_intermediate/day17/main.py
+++ 02_intermediate/day17/main.py
@@ -1,12 +1,16 @@
 from data import question_data
+from open_trivia import data
 from question_model import Question
 from quiz_brain import QuizBrain
 
 if __name__ == '__main__':
     question_bank = []
 
-    for question in question_data:
-        question_bank.append(Question(question['text'], question['answer']))
+    # for question in question_data:
+    #     question_bank.append(Question(question['text'], question['answer']))
+
+    for question in data:
+        question_bank.append(Question(question['question'], question['correct_answer']))
 
     quiz = QuizBrain(question_bank)
 
