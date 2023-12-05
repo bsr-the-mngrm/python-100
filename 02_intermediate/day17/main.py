a12a0b98ccd95fa662bb941a780d80429d020dfd
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-05 20:27:57 +0100

    Update main.py

--- 02_intermediate/day17/main.py
+++ 02_intermediate/day17/main.py
@@ -12,3 +12,6 @@ if __name__ == '__main__':
 
     while quiz.still_has_questions():
         quiz.next_question()
+
+    print("\nYou've completed the quiz")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
