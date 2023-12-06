bd857330db110a5dfc40060960618bd73c330c7c
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-06 22:53:32 +0100

    Update main.py

--- 02_intermediate/day18/main.py
+++ 02_intermediate/day18/main.py
@@ -1,10 +1,16 @@
-import turtle
+import turtle as t
 
-if __name__ == '__main__':
-    timmy_the_turtle = turtle.Turtle()
-    timmy_the_turtle.shape("turtle")
-    timmy_the_turtle.color("BlueViolet")
-    timmy_the_turtle.forward(100)
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
 
-    screen = turtle.Screen()
-    screen.exitonclick()
+screen = t.Screen()
+screen.exitonclick()
