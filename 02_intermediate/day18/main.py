136939a2fb0a25af063d140174845b712abde66e
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-07 17:44:09 +0100

    Update main.py

--- 02_intermediate/day18/main.py
+++ 02_intermediate/day18/main.py
@@ -2,17 +2,27 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+t.colormode(255)
 
-turtle_colors = ["azure", "lime green", "forest green", "olive", "gold", "wheat", "firebrick", "deep pink", "purple",
-                 "blue violet", "lavender", "slate blue", "gray", "deep sky blue", "sienna"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
+# turtle_colors = ["azure", "lime green", "forest green", "olive", "gold", "wheat", "firebrick", "deep pink", "purple",
+#                  "blue violet", "lavender", "slate blue", "gray", "deep sky blue", "sienna"]
+
 
 directions = [0, 90, 180, 270]
 
-tim.pensize(10)
+tim.pensize(5)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(turtle_colors))
+    tim.color(random_color())
     tim.setheading(random.choice(directions))
     tim.forward(20)
 
