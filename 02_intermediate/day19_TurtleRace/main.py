3bda5bd2d04db2f669b43e060d1c39e4bff489ba
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-08 13:13:03 +0100

    Update main.py

--- 02_intermediate/day19_TurtleRace/main.py
+++ 02_intermediate/day19_TurtleRace/main.py
@@ -1,7 +1,39 @@
+import turtle
 import turtle as t
+import random
 
-if __name__ == '__main__':
-    tim = t.Turtle()
-    screen = t.Screen()
+is_race_on = False
+turtles = []
+colors = ["blue", "red", "yellow", "green", "purple"]
 
-    screen.listen()
+screen = t.Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt=f"Which turtle will win the race?\n{colors}\nEnter a color:")
+
+for i in range(5):
+    new_turtle = t.Turtle()
+    new_turtle.color(colors[i])
+    new_turtle.shape("turtle")
+    new_turtle.penup()
+    x = -230
+    y = -100 + 30*i
+    new_turtle.goto(x=x, y=y)
+    turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            is_race_on = False
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
+
+
+screen.exitonclick()
