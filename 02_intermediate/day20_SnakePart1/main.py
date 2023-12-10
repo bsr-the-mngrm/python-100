7f3e55e763f0b4f38427c8d89c8b424c01451244
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 17:48:23 +0100

    Update main.py

--- 02_intermediate/day20_SnakePart1/main.py
+++ 02_intermediate/day20_SnakePart1/main.py
@@ -1,18 +1,23 @@
 import turtle as t
 import snake as s
+import time
 
 if __name__ == '__main__':
     screen = t.Screen()
     screen.setup(width=600, height=600)
     screen.bgcolor("black")
     screen.title("My Snake Game")
+    screen.tracer(0)
 
     my_snake = s.Snake()
+
+    screen.update()
 
     game_is_on = True
 
     while game_is_on:
-        for body_part in my_snake.snake_body:
-            body_part.forward(20)
+        my_snake.move_forward()
+        time.sleep(0.2)
+        screen.update()
 
     screen.exitonclick()
