80ee059c4da0247621b2f086f8d52b0f3b2ab51a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 17:32:38 +0100

    Update main.py

--- 02_intermediate/day20_SnakePart1/main.py
+++ 02_intermediate/day20_SnakePart1/main.py
@@ -9,4 +9,10 @@ if __name__ == '__main__':
 
     my_snake = s.Snake()
 
+    game_is_on = True
+
+    while game_is_on:
+        for body_part in my_snake.snake_body:
+            body_part.forward(20)
+
     screen.exitonclick()
