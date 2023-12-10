90d0d43ed4ad7b079219b5a9053d647e90a0f420
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 21:56:43 +0100

    Update scoreboard.py

--- 02_intermediate/day_21_SnakePart2/scoreboard.py
+++ 02_intermediate/day_21_SnakePart2/scoreboard.py
@@ -10,5 +10,5 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.color("white")
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
         self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
