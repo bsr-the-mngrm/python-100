d081be660f74b0fd403fe37ce25070913f87aacf
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 18:58:12 +0100

    Update snake.py

--- 02_intermediate/day20_SnakePart1/snake.py
+++ 02_intermediate/day20_SnakePart1/snake.py
@@ -10,6 +10,10 @@ class Snake:
     def __init__(self):
         self.game_is_on = True
         self.snake_body = []
+        self.__create_snake()
+        self.head = self.snake_body[0]
+
+    def __create_snake(self):
         for position in STARTING_POSITIONS:
             new_snake_part = t.Turtle(shape="square")
             new_snake_part.color("white")
@@ -23,30 +27,27 @@ class Snake:
             new_x = self.snake_body[body_part_num-1].xcor()
             new_y = self.snake_body[body_part_num-1].ycor()
             self.snake_body[body_part_num].goto(x=new_x, y=new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def move_up(self):
         """Move snake forward"""
+        self.head.setheading(90)
         self.__move()
-        self.snake_body[0].setheading(90)
-        self.snake_body[0].forward(MOVE_DISTANCE)
 
     def move_down(self):
         """Move snake forward"""
+        self.head.setheading(270)
         self.__move()
-        self.snake_body[0].setheading(270)
-        self.snake_body[0].forward(MOVE_DISTANCE)
 
     def move_right(self):
         """Turn snake right"""
+        self.head.setheading(0)
         self.__move()
-        self.snake_body[0].setheading(0)
-        self.snake_body[0].forward(MOVE_DISTANCE)
 
     def move_left(self):
         """Turn snake left"""
+        self.head.setheading(180)
         self.__move()
-        self.snake_body[0].setheading(180)
-        self.snake_body[0].forward(MOVE_DISTANCE)
 
     def stop_game(self):
         self.game_is_on = False
