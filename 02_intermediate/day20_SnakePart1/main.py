0314aac478d63a7cba602b739ca658d8276f5834
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 16:36:55 +0100

    init screen

--- 02_intermediate/day20_SnakePart1/main.py
+++ 02_intermediate/day20_SnakePart1/main.py
@@ -1,3 +1,9 @@
+import turtle as t
 
 if __name__ == '__main__':
-    pass
+    screen = t.Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+
+    screen.exitonclick()
