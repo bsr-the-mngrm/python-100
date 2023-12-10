b728f958421daddac83ccfd5cffe8f24d1fa5e97
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 17:28:37 +0100

    Create my_snake in main.py

--- 02_intermediate/day20_SnakePart1/main.py
+++ 02_intermediate/day20_SnakePart1/main.py
@@ -1,9 +1,12 @@
 import turtle as t
+import snake as s
 
 if __name__ == '__main__':
     screen = t.Screen()
     screen.setup(width=600, height=600)
     screen.bgcolor("black")
     screen.title("My Snake Game")
+
+    my_snake = s.Snake()
 
     screen.exitonclick()
