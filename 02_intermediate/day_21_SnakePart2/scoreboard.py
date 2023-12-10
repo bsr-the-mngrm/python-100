8f5a125c915c8ad0c79b3809c8ffad00f864bcdb
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-10 22:02:05 +0100

    Add scoreboard.update_scoreboard function

--- 02_intermediate/day_21_SnakePart2/scoreboard.py
+++ 02_intermediate/day_21_SnakePart2/scoreboard.py
@@ -11,9 +11,12 @@ class Scoreboard(Turtle):
         self.penup()
         self.color("white")
         self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.update_scoreboard()
