9ff131f15155e7833a06b619656d177eb60d8d52
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-11 10:57:03 +0100

    Create the screen

--- 02_intermediate/day22_Pong/main.py
+++ 02_intermediate/day22_Pong/main.py
@@ -1,3 +1,10 @@
+from turtle import Screen
+
 
 if __name__ == '__main__':
-    pass
+    screen = Screen()
+    screen.setup(width=800, height=600)
+    screen.bgcolor("black")
+    screen.title("Pong")
+
+    screen.exitonclick()
