61c18a2b8aa3b18795fb1689101a813863fba788
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-11 11:22:23 +0100

    Create Paddle class

--- 02_intermediate/day22_Pong/paddle.py
+++ 02_intermediate/day22_Pong/paddle.py
@@ -1,12 +1,29 @@
 from turtle import Turtle
 
+UP = 90
+DOWN = 270
+STEP_DISTANCE = 50
+
 
 class Paddle(Turtle):
 
     def __init__(self, position: tuple[float, float]):
         super().__init__(shape="square")
         self.color("white")
-        self.shapesize(stretch_wid=20, stretch_len=100, outline=None)
+        self.shapesize(stretch_wid=1, stretch_len=5, outline=None)
         self.penup()
         self.goto(position)
-        
+        self.setheading(UP)
+
+        self.game_is_on = True
+
+    def go_up(self):
+        self.setheading(UP)
+        self.forward(STEP_DISTANCE)
+
+    def go_down(self):
+        self.setheading(DOWN)
+        self.forward(STEP_DISTANCE)
+
+    def game_over(self):
+        self.game_is_on = False
