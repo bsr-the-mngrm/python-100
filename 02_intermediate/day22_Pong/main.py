9e266373c958ded514ac9ad27ad0feb7ff7ffe88
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-11 11:22:46 +0100

    Create paddle instances

--- 02_intermediate/day22_Pong/main.py
+++ 02_intermediate/day22_Pong/main.py
@@ -1,5 +1,8 @@
-from turtle import Screen
+from turtle import Screen, bye
+from paddle import Paddle
 
+PLAYER_PADDLE_POSITION = (350, 0)
+COMPUTER_PADDLE_POSITION = (-350, 0)
 
 if __name__ == '__main__':
     screen = Screen()
@@ -7,4 +10,18 @@ if __name__ == '__main__':
     screen.bgcolor("black")
     screen.title("Pong")
 
+    my_paddle = Paddle(PLAYER_PADDLE_POSITION)
+    computer_paddle = Paddle(COMPUTER_PADDLE_POSITION)
+
+    screen.tracer(0)
+
+    screen.listen()
+    screen.onkey(my_paddle.go_up, "Up")
+    screen.onkey(my_paddle.go_down, "Down")
+    screen.onkey(my_paddle.game_over, "Escape")
+
+    while my_paddle.game_is_on:
+        screen.update()
+
+    screen.onkey(bye, "Escape")
     screen.exitonclick()
