dd6eeb149b23e79c6931f5219da9269cbeebf5e4
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-11 12:48:07 +0100

    Refactor bounce-back

--- 02_intermediate/day22_Pong/ball.py
+++ 02_intermediate/day22_Pong/ball.py
@@ -1,50 +1,24 @@
 from turtle import Turtle
 
-UP_RIGHT = 45
-UP_LEFT = 135
-DOWN_LEFT = 225
-DOWN_RIGHT = 315
-
-TOP_WALL = 270
-BOTTOM_WALL = -270
-RIGHT_WALL = 370
-LEFT_WALL = -370
-
-STEP_DISTANCE = 50
-
 
 class Ball(Turtle):
-    
+
     def __init__(self):
-        super().__init__(shape="circle")
+        super().__init__()
         self.color("white")
-        self.shapesize(stretch_wid=1, stretch_len=1, outline=None)
+        self.shape("circle")
         self.penup()
-        self.setheading(DOWN_LEFT)
-
-    def __check_wall_collisions(self):
-        if self.ycor() > TOP_WALL:
-            if self.heading() == UP_RIGHT:
-                self.setheading(DOWN_RIGHT)
-            else:
-                self.setheading(DOWN_LEFT)
-        elif self.ycor() < BOTTOM_WALL:
-            if self.heading() == DOWN_RIGHT:
-                self.setheading(UP_RIGHT)
-            else:
-                self.setheading(UP_LEFT)
-
-        if self.xcor() > RIGHT_WALL:
-            if self.heading() == UP_RIGHT:
-                self.setheading(UP_LEFT)
-            else:
-                self.setheading(DOWN_LEFT)
-        elif self.xcor() < LEFT_WALL:
-            if self.heading() == UP_LEFT:
-                self.setheading(UP_RIGHT)
-            else:
-                self.setheading(DOWN_RIGHT)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        self.__check_wall_collisions()
-        self.forward(STEP_DISTANCE)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- 02_intermediate/day22_Pong/main.py
+++ 02_intermediate/day22_Pong/main.py
@@ -11,12 +11,11 @@ if __name__ == '__main__':
     screen.setup(width=800, height=600)
     screen.bgcolor("black")
     screen.title("Pong")
+    screen.tracer(0)
 
     my_paddle = Paddle(PLAYER_PADDLE_POSITION)
     computer_paddle = ComputerPaddle(COMPUTER_PADDLE_POSITION)
     ball = Ball()
-
-    screen.tracer(0)
 
     screen.listen()
     screen.onkey(my_paddle.go_up, "Up")
@@ -29,6 +28,15 @@ if __name__ == '__main__':
 
         ball.move()
         computer_paddle.move()
+
+        # Detect collision with wall
+        if ball.ycor() > 280 or ball.ycor() < -280:
+            ball.bounce_y()
+
+        # Detect collision with paddle
+        if (ball.distance(my_paddle) < 50 and ball.xcor() > 320
+                or ball.distance(computer_paddle) < 50 and ball.xcor() < -320):
+            ball.bounce_x()
 
     screen.onkey(bye, "Escape")
     screen.exitonclick()
