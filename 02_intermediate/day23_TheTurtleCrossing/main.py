c4395848be1b25eac3529f05fc9e374d748bad76
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-12 16:51:03 +0100

    Update main.py

--- 02_intermediate/day23_TheTurtleCrossing/main.py
+++ 02_intermediate/day23_TheTurtleCrossing/main.py
@@ -8,7 +8,17 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=player.move)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player.check_finish_line():
+        scoreboard.update_level()
+        player.update_level()
