023ddb1498b8e5bc3fa0899559c7322f1562c25c
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-12 16:43:03 +0100

    Define Scoreboard class

--- 02_intermediate/day23_TheTurtleCrossing/scoreboard.py
+++ 02_intermediate/day23_TheTurtleCrossing/scoreboard.py
@@ -1,7 +1,24 @@
 from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
+LEVEL_POSITION = (-250, 280)
 
+class Scoreboard(Turtle):
 
-class Scoreboard:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.goto(LEVEL_POSITION)
+        self.update_level()
+
+    def update_level(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def game_over(self):
+        self.home()
+        self.write("GAME OVER", align="center", font=FONT)
