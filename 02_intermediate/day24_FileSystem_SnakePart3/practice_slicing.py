82e3ad983cab5d536bb5f5107053e3ffd629d34d
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-13 19:44:01 +0100

    Clean up day24 files

--- 02_intermediate/day24_FileSystem_SnakePart3/practice_slicing.py
+++
@@ -1,7 +0,0 @@
-piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
-piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
-
-print(piano_tuple[::-1])
-print(piano_tuple[::2])
-print(piano_tuple[1::2])
-print(piano_keys[2:5])
