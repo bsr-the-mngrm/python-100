3ef497758adbed3519e635f914a563956782a186
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-14 00:21:14 +0100

    Comleted the day24 challenge (mail merge)

--- 02_intermediate/day24_mail-merge-project/main.py
+++ 02_intermediate/day24_mail-merge-project/main.py
@@ -1,3 +1,41 @@
+PLACEHOLDER = "[name]"
+
+
+def open_starting_letter():
+    letter = ""
+
+    with open("Input/Letters/starting_letter.txt") as file:
+        letter = file.read()
+
+    return letter
+
+
+def open_invited_names():
+    names = []
+    with open("Input/Names/invited_names.txt") as file:
+        for name in file.readlines():
+            new_name = name.strip()
+            names.append(new_name)
+
+    return names
+
+
+def write_letters(letter: str, names: list):
+    for name in names:
+        new_letter = letter.replace(PLACEHOLDER, name)
+        with open(f"Output/ReadyToSend/letter_to_{name.lower()}", mode="w") as file:
+            file.write(new_letter)
+
 
 if __name__ == '__main__':
-    pass
+    starting_letter = open_starting_letter()
+    print("starting_letter.txt loaded\n")
+
+    invited_names = open_invited_names()
+    print("invited_names.txt loaded\n")
+
+    write_letters(starting_letter, invited_names)
+    print("Letters are written! Check out 'Output/ReadyToSend' folder!")
+
+
+
