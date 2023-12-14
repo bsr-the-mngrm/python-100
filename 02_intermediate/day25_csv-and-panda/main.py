26a22482951f7c92aa1c146b77197f6d25f8c3c4
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-14 12:09:27 +0100

    Add first part of US States Game

--- 02_intermediate/day25_csv-and-panda/main.py
+++ 02_intermediate/day25_csv-and-panda/main.py
@@ -1,3 +1,20 @@
+import turtle
+
+# # Get x and y coordinates of a click on the screen
+# def get_mouse_click_cor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_cor)
+# turtle.mainloop()
 
 if __name__ == '__main__':
-    pass
+    screen = turtle.Screen()
+    screen.title("U.S. States Game")
+    image_path = "img/blank_states_img.gif"
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+    screen.exitonclick()
