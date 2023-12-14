24356c4d4a1b66178a9a1ef11bb88f864cc2dfb0
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-14 18:09:24 +0100

    Add second part of 50 states game

--- 02_intermediate/day25_csv-and-panda/main.py
+++ 02_intermediate/day25_csv-and-panda/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 # # Get x and y coordinates of a click on the screen
 # def get_mouse_click_cor(x, y):
@@ -15,6 +16,40 @@ if __name__ == '__main__':
     screen.addshape(image_path)
     turtle.shape(image_path)
 
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    data = pandas.read_csv("csv/50_states.csv")
+    states = data["state"].to_list()
+    number_of_states = len(states)
+    guessed_states = []
+    number_of_guessed_states = 50 #len(guessed_states)
+    answer_state = ""
 
-    screen.exitonclick()
+    state_writer = turtle.Turtle()
+    state_writer.color("black")
+    state_writer.penup()
+    state_writer.hideturtle()
+
+    # new_york = data[states["state"] == "New York"]
+    # new_york.reset_index(drop=True)
+    # x = new_york.x.iloc[0]
+    # print(x)
+
+    while number_of_guessed_states != number_of_states and answer_state != "exit":
+        answer_state = screen.textinput(title=f"{number_of_guessed_states}/{number_of_states} States Correct",
+                                        prompt="What's another state's name?").title()
+
+        if answer_state in states and answer_state not in guessed_states:
+            guessed_states.append(answer_state)
+            number_of_guessed_states += 1
+
+            state_data = data[data["state"] == answer_state]
+            state_x = state_data["x"].iloc[0]
+            state_y = state_data["y"].iloc[0]
+            state_writer.goto(state_x, state_y)
+            state_writer.write(answer_state, align="center", font=("Arial", 8, "normal"))
+
+    if number_of_guessed_states == number_of_states:
+        name_of_winner = screen.textinput(title="Game Over", prompt="Congrats! Save your name in the winner's list:")
+        with open("csv/50_states_winners.csv", mode="a") as file:
+            file.write(f"{name_of_winner}\n")
+
+    # screen.exitonclick()
