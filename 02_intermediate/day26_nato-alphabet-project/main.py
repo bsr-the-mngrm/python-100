4d7c754591b068622a39a7b2f79224eb917a26e1
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-15 14:28:45 +0100

    Update main.py

--- 02_intermediate/day26_nato-alphabet-project/main.py
+++ 02_intermediate/day26_nato-alphabet-project/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_df.iterrows()}
 
     # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-    user_input = input("Write a word: ")
+    user_input = input("Enter a word: ")
     user_input_letters = [letter for letter in user_input]
     user_input_with_codes = {letter: nato_alphabet_dict[letter.upper()] for letter in user_input_letters}
 
