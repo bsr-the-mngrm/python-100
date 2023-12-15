d94cc2b7016bd7da87409eb40bd4e3b22985d22d
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-15 13:16:31 +0100

    Create practice-list-comprehension.py

---
+++ 02_intermediate/day26_nato-alphabet-project/practice-list-comprehension.py
@@ -0,0 +1,25 @@
+# # TASK1
+# numbers = [1, 2, 3]
+# new_list = [n+1 for n in numbers]
+# print(new_list)
+
+# # TASK2
+# name = "Angela"
+# new_list = [letter.capitalize() for letter in name]
+# print(new_list)
+
+# # TASK3
+# new_list = [num*2 for num in range(1, 5)]
+# print(new_list)
+
+# # TASK4
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# short_names = [name for name in names if len(name) < 5]
+# print(short_names)
+
+# # TASK5
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# long_names = [name.upper() for name in names if len(name) > 5]
+# print(long_names)
+
+
