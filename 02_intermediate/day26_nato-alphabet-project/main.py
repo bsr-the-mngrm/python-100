e2e07c109c68f80570e3a53c15d6f27a4145e1e7
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-15 14:25:50 +0100

    Update main.py

--- 02_intermediate/day26_nato-alphabet-project/main.py
+++ 02_intermediate/day26_nato-alphabet-project/main.py
@@ -6,6 +6,12 @@ if __name__ == '__main__':
 
     # TODO 1. Create a dictionary in this format:
     # {"A": "Alfa", "B": "Bravo"}
-    
-    # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+    nato_alphabet_df = pandas.read_csv("csv/nato_phonetic_alphabet.csv")
+    nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_df.iterrows()}
 
+    # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+    user_input = input("Write a word: ")
+    user_input_letters = [letter for letter in user_input]
+    user_input_with_codes = {letter: nato_alphabet_dict[letter.upper()] for letter in user_input_letters}
+
+    print(user_input_with_codes)
