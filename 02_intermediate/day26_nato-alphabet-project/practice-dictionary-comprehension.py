dfbd389d65592b2aae0daa5192851d915ac0a4b7
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-15 14:00:38 +0100

    Create practice-dictionary-comprehension.py

---
+++ 02_intermediate/day26_nato-alphabet-project/practice-dictionary-comprehension.py
@@ -0,0 +1,18 @@
+# TASK1
+import random
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+student_scores = {name: random.randint(0, 100) for name in names}
+print(student_scores)
+print(student_scores["Alex"])
+passed_students = {student: score for (student, score) in student_scores.items() if score > 60}
+print(passed_students)
+
+# TASK2
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word: len(word) for word in sentence.split(" ")}
+print(result)
+
+# TASK3
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {day: ((temp_c * 9/5)+32) for (day, temp_c) in weather_c.items()}
+print(weather_f)
