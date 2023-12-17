defaeaa3ec480b09eb979a0537a707e81c74d18b
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-17 19:44:42 +0100

    Update practice_tkinter.py

--- 02_intermediate/day27_Tkinter-and-args/practice_tkinter.py
+++ 02_intermediate/day27_Tkinter-and-args/practice_tkinter.py
@@ -11,20 +11,30 @@ if __name__ == '__main__':
 
     window.title("Hello World w/ GUI")
     window.minsize(width=500, height=300)
+    # set padding:
+    window.config(padx=20, pady=20)
 
     # Label
     my_label = Label(text="I am a Label!", font=('Ariel', 24, "normal"))
-    my_label.pack()
+    # my_label.pack()
+    # my_label.place(x=100, y=200)
+    my_label.grid(column=0, row=0)
 
     my_label["text"] = "New text"
     my_label.config(text="Third Text")
 
     # Button
     button = Button(text="Click Me", command=button_clicked)
-    button.pack()
+    # button.pack()
+    button.grid(column=0, row=1)
+
+    button2 = Button(text="Don't Click Me", command=button_clicked)
+    # button.pack()
+    button2.grid(column=1, row=1)
 
     # Textbox/Entry
     textbox = Entry(width=10)
-    textbox.pack()
+    # textbox.pack()
+    textbox.grid(column=0, row=2)
 
     window.mainloop()
