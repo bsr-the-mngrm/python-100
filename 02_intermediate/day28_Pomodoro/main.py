652c3de26d344ffb4c5569cdd38348ff16215e25
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-18 21:39:53 +0100

    Set the background

--- 02_intermediate/day28_Pomodoro/main.py
+++ 02_intermediate/day28_Pomodoro/main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import time
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,12 +11,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
 
 if __name__ == '__main__':
-    # ---------------------------- TIMER RESET ------------------------------- #
-    pass
-    # ---------------------------- TIMER MECHANISM ------------------------------- #
-
-    # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
     # ---------------------------- UI SETUP ------------------------------- #
+    window = Tk()
+    window.title("Pomodoro")
+    window.config(padx=100, pady=50, bg=YELLOW)
+
+    # Tomato image settings
+    canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+    tomato_photo = PhotoImage(file="img/tomato.png")
+    canvas.create_image(100, 112, image=tomato_photo)
+    canvas.pack()
+
+    window.mainloop()
