d4393d6ca66e459794ca1e40cfb91ab033822619
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-19 21:41:27 +0100

    Add Password Manager logo

--- 02_intermediate/day29_PasswordManager/main.py
+++ 02_intermediate/day29_PasswordManager/main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -5,4 +7,14 @@
 
 if __name__ == '__main__':
     # ---------------------------- UI SETUP ------------------------------- #
-    pass
+    window = Tk()
+    window.title("Password Manager")
+    window.config(padx=20, pady=20)
+
+    # Password Manager logo
+    canvas = Canvas(width=200, height=189, highlightthickness=0)
+    logo_img = PhotoImage(file="img/logo.png")
+    canvas.create_image(100, 94, image=logo_img)
+    canvas.grid(column=0, row=0)
+
+    window.mainloop()
