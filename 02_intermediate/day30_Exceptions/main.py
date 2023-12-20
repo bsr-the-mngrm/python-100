aafc32b69864e782216dd1c43ee4442766a015f0
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-20 15:13:42 +0100

    Add built-in exceptions

--- 02_intermediate/day30_Exceptions/main.py
+++ 02_intermediate/day30_Exceptions/main.py
@@ -1,12 +1,22 @@
 
 if __name__ == '__main__':
-    # # FileNotFound
-    # with open(".data/b_file.txt") as file:
-    #     file.read()
+    # FileNotFound
+    try:
+        file = open(".data/b_file.txt", mode="r")
 
-    # # KeyError
-    # a_dictionary = {"key": "value"}
-    # value = a_dictionary["non_existent_key"]
+        # KeyError
+        a_dictionary = {"key": "value"}
+        value = a_dictionary["key"]
+    except FileNotFoundError:
+        with open(".data/b_file.txt", mode="a") as file:
+            file.write("Something")
+    except KeyError as error_message:
+        print(f"The key {error_message} key does not exist.")
+    else:
+        content = file.read()
+        print(content)
+    finally:
+        file.close()
 
     # # IndexError
     # fruit_list = ["Apple", "Banana", "Pear"]
