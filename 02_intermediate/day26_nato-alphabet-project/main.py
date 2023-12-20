c353fcf21b3bf5624ea126c2ed7a3778726d668d
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-20 15:52:57 +0100

    Add exception handling to NATO Phonetic Alphabet project

--- 02_intermediate/day26_nato-alphabet-project/main.py
+++ 02_intermediate/day26_nato-alphabet-project/main.py
@@ -1,6 +1,21 @@
 import pandas
 
 
+def generate_phonetic():
+    # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+    user_input = input("Enter a word: ").upper()
+    user_input_chars = [char for char in user_input]
+
+    # TODO 3. Add exception handling: if there is a non-alfabet character in the input catch the KeyError exception
+    try:
+        user_input_with_codes = {letter: nato_alphabet_dict[letter.upper()] for letter in user_input_chars}
+    except KeyError:
+        print("Only letters are acceptable!")
+        generate_phonetic()
+    else:
+        print(user_input_with_codes)
+
+
 if __name__ == '__main__':
     print("Welcome to NATO Alphabet Project")
 
@@ -9,9 +24,4 @@ if __name__ == '__main__':
     nato_alphabet_df = pandas.read_csv("csv/nato_phonetic_alphabet.csv")
     nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_df.iterrows()}
 
-    # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-    user_input = input("Enter a word: ").upper()
-    user_input_letters = [letter for letter in user_input]
-    user_input_with_codes = {letter: nato_alphabet_dict[letter.upper()] for letter in user_input_letters}
-
-    print(user_input_with_codes)
+    generate_phonetic()
