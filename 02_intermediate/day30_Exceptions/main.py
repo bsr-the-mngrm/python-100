18777fffcbf74046b1eefde3c44fe3d4df17fa70
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-20 14:54:33 +0100

    Typical errors

--- 02_intermediate/day30_Exceptions/main.py
+++ 02_intermediate/day30_Exceptions/main.py
@@ -1,3 +1,17 @@
 
 if __name__ == '__main__':
-    pass
+    # # FileNotFound
+    # with open(".data/b_file.txt") as file:
+    #     file.read()
+
+    # # KeyError
+    # a_dictionary = {"key": "value"}
+    # value = a_dictionary["non_existent_key"]
+
+    # # IndexError
+    # fruit_list = ["Apple", "Banana", "Pear"]
+    # fruit = fruit_list[3]
+
+    # # TypeError
+    # text = "abc"
+    # print(text + 3)
