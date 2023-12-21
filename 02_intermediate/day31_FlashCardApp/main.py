0a29c06f26aa8b867de1b6df30021c6d15f8ef0a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-21 16:01:21 +0100

    Add UI setup

---
+++ 02_intermediate/day31_FlashCardApp/main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+if __name__ == '__main__':
+    # ----------------------- UI SETUP ----------------------- #
+    window = Tk()
+    window.title("Flash Card App")
+    window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+    card_back_img = PhotoImage(file="images/card_back.png")
+    card_front_img = PhotoImage(file="images/card_front.png")
+    check_mark_img = PhotoImage(file="images/right.png")
+    cross_mark_img = PhotoImage(file="images/wrong.png")
+
+    # Flash Card
+    canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+    canvas.create_image(400, 263, image=card_back_img)
+    canvas.grid(column=0, row=0, columnspan=2)
+    canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"), tags="language")
+    canvas.create_text(400, 263, text="ui", font=("Ariel", 60, "bold"), tags="word")
+
+    # Cross mark button
+    cross_mark_btn = Button(image=cross_mark_img, highlightthickness=0)
+    cross_mark_btn.grid(column=0, row=1)
+
+    # Check mark button
+    check_mark_btn = Button(image=check_mark_img, highlightthickness=0)
+    check_mark_btn.grid(column=1, row=1)
+
+    window.mainloop()
