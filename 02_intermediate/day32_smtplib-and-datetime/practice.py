35308b488dbbce6a387dea040bb33905effeff7a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 22:19:05 +0100

    Add 'with' statement syntax

--- 02_intermediate/day32_smtplib-and-datetime/practice.py
+++ 02_intermediate/day32_smtplib-and-datetime/practice.py
@@ -8,11 +8,10 @@ sender_email = os.getenv('SENDER_EMAIL')
 sender_password = os.getenv('SENDER_APP_KEY')
 recipient_email = os.getenv('RECIPIENT_EMAIL')
 
-connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-connection.login(user=sender_email, password=sender_password)
-connection.sendmail(
-    from_addr=sender_email,
-    to_addrs=recipient_email,
-    msg="Subject: Testing\n\nHello SMTP!"
-)
-connection.close()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+    connection.login(user=sender_email, password=sender_password)
+    connection.sendmail(
+        from_addr=sender_email,
+        to_addrs=recipient_email,
+        msg="Subject: Testing\n\nHello SMTP!"
+    )
