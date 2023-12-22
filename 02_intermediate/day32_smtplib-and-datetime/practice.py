05fef30994d124807542173c082fe1e77c9e9e7e
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 22:27:16 +0100

    Add datetime practice

--- 02_intermediate/day32_smtplib-and-datetime/practice.py
+++ 02_intermediate/day32_smtplib-and-datetime/practice.py
@@ -1,17 +1,28 @@
-import smtplib
-import os
-from dotenv import load_dotenv
+# import smtplib
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv('env/.env')
+#
+# sender_email = os.getenv('SENDER_EMAIL')
+# sender_password = os.getenv('SENDER_APP_KEY')
+# recipient_email = os.getenv('RECIPIENT_EMAIL')
+#
+# with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+#     connection.login(user=sender_email, password=sender_password)
+#     connection.sendmail(
+#         from_addr=sender_email,
+#         to_addrs=recipient_email,
+#         msg="Subject: Testing\n\nHello SMTP!"
+#     )
 
-load_dotenv('env/.env')
+import datetime as dt
 
-sender_email = os.getenv('SENDER_EMAIL')
-sender_password = os.getenv('SENDER_APP_KEY')
-recipient_email = os.getenv('RECIPIENT_EMAIL')
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
-    connection.login(user=sender_email, password=sender_password)
-    connection.sendmail(
-        from_addr=sender_email,
-        to_addrs=recipient_email,
-        msg="Subject: Testing\n\nHello SMTP!"
-    )
+date_of_birth = dt.datetime(year=1900, month=1, day=1, hour=4, minute=49)
+print(date_of_birth)
