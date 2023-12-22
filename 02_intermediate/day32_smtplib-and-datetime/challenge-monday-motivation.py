af5e520a10a3d9b1f7bd93a9f1d1cc53b2551c8f
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 22:43:06 +0100

    Add 'read_quotes' function

--- 02_intermediate/day32_smtplib-and-datetime/challenge-monday-motivation.py
+++ 02_intermediate/day32_smtplib-and-datetime/challenge-monday-motivation.py
@@ -1,6 +1,18 @@
 from datetime import datetime
 
-day_of_week = datetime.now().weekday()
 
-if day_of_week == 4:
-    pass
+def read_quotes():
+    with (open("data/quotes.txt", mode='r') as quotes_file):
+        qs = []
+        for row in quotes_file.readlines():
+            qs.append(row.strip())
+    return qs
+
+
+if __name__ == '__main__':
+    quotes = read_quotes()
+    
+    day_of_week = datetime.now().weekday()
+
+    if day_of_week == 4:
+        pass
