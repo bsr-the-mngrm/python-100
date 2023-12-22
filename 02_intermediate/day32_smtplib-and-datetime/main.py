d375bab07b4fd6f7bf42504d7b39195898d50898
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 23:46:09 +0100

    Add 'generate_birthday_message' function

--- 02_intermediate/day32_smtplib-and-datetime/main.py
+++ 02_intermediate/day32_smtplib-and-datetime/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime
+from random import randint
 import pandas
 
 
 def read_birthdays():
     return pandas.read_csv(".data/birthdays.csv").to_dict(orient="records")
+
+
+def generate_birthday_message(name: str):
+    n = randint(1, 3)
+    with open(f".data/letter_templates/letter_{n}.txt") as letter:
+        return "".join(letter.readlines()).replace("[NAME]", name)
 
 
 if __name__ == '__main__':
@@ -12,4 +19,4 @@ if __name__ == '__main__':
 
     for birthday in birthdays:
         if birthday['month'] == today.month and birthday['day'] == today.day:
-            pass
+            msg = generate_birthday_message(birthday['name'])
