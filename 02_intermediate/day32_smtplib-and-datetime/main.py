6b690c72c95895ef80d2193d2c50313d14c4a68a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 23:22:44 +0100

    Start of Automated Birthday Wisher project

--- 02_intermediate/day32_smtplib-and-datetime/main.py
+++ 02_intermediate/day32_smtplib-and-datetime/main.py
@@ -1,3 +1,9 @@
+import pandas
+
+
+def read_birthdays():
+    return pandas.read_csv(".data/birthdays.csv").to_dict(orient="records")
+
 
 if __name__ == '__main__':
-    pass
+    birthdays = read_birthdays()
