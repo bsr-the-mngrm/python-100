57b860e5b286c6d9cfcec0ce1556073cdeec64a5
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 23:47:36 +0100

    Add 'send_birthday_wish' function

--- 02_intermediate/day32_smtplib-and-datetime/main.py
+++ 02_intermediate/day32_smtplib-and-datetime/main.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from random import randint
+from dotenv import load_dotenv
 import pandas
+import os
+import smtplib
 
 
 def read_birthdays():
@@ -13,6 +16,23 @@ def generate_birthday_message(name: str):
         return "".join(letter.readlines()).replace("[NAME]", name)
 
 
+def send_birthday_wish(birthday_msg):
+    load_dotenv('env/.env')
+
+    sender_email = os.getenv('SENDER_EMAIL')
+    sender_password = os.getenv('SENDER_APP_KEY')
+    recipient_email = os.getenv('RECIPIENT_EMAIL')
+
+    with smtplib.SMTP_SSL(host='smtp.gmail.com', port=465) as connection:
+        connection.login(user=sender_email, password=sender_password)
+        connection.sendmail(
+            from_addr=sender_email,
+            to_addrs=recipient_email,
+            msg=f"Subject:Happy Birthday!\n\n"
+                f"{birthday_msg}"
+        )
+
+
 if __name__ == '__main__':
     birthdays = read_birthdays()
     today = datetime.now()
@@ -20,3 +40,4 @@ if __name__ == '__main__':
     for birthday in birthdays:
         if birthday['month'] == today.month and birthday['day'] == today.day:
             msg = generate_birthday_message(birthday['name'])
+            send_birthday_wish(msg)
