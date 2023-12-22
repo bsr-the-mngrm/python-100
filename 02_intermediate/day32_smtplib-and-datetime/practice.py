74240a97c1f4b8653fbe4ff3f74cf1f10433d41e
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-22 22:10:44 +0100

    Create practice.py

---
+++ 02_intermediate/day32_smtplib-and-datetime/practice.py
@@ -0,0 +1,14 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv('env/.env')
+
+sender_email = os.getenv('SENDER_EMAIL')
+sender_password = os.getenv('SENDER_APP_KEY')
+recipient_email = os.getenv('RECIPIENT_EMAIL')
+
+connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+connection.login(user=sender_email, password=sender_password)
+connection.sendmail(from_addr=sender_email, to_addrs=recipient_email, msg="Hello SMTP!")
+connection.close()
