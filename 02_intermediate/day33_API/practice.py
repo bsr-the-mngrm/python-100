d8691ad04039142458680733d38ed317767c199a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-23 21:52:22 +0100

    Update practice.py

--- 02_intermediate/day33_API/practice.py
+++ 02_intermediate/day33_API/practice.py
@@ -1,5 +1,8 @@
 import requests
 
+MY_LAT = 47.192087
+MY_LNG = 20.188501
+
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
@@ -18,3 +21,19 @@ iss_position = (longitude, latitude)
 
 print(iss_position)
 
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data['results']['sunrise'].split("T")[1].split("+")[0]
+sunset = data['results']['sunset'].split("T")[1].split("+")[0]
+
+print(data)
+print(sunrise[:5])
+print(sunset[:5])
