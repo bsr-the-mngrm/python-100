6dd7a05f1814429025ddb0565d065420045175fe
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-23 23:16:46 +0100

    Update main.py

--- 02_intermediate/day33_API/main.py
+++ 02_intermediate/day33_API/main.py
@@ -1,34 +1,39 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351  # Your latitude
-MY_LONG = -0.127758  # Your longitude
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-# Your position is within +5 or -5 degrees of the ISS position.
+MY_LAT = 47.192087
+MY_LNG = 20.188501
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-time_now = datetime.now()
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <= iss_latitude <= MY_LNG+5 and MY_LNG-5 <= iss_longitude <= MY_LNG+5:
+        return True
 
-# If the ISS is close to my current position,
-# and it is currently dark
-# Then email me to tell me to look up.
-# BONUS: run the code every 60 seconds.
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+
+    if time_now.hour >= sunset or time_now.hour <= sunrise:
+        return True
+
+
