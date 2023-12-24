8b1f243533806d3e1a9e4a07e701c596adc5a235
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-25 00:45:08 +0100

    Add STEP 2

--- 02_intermediate/day36_StockNews/main.py
+++ 02_intermediate/day36_StockNews/main.py
@@ -8,6 +8,8 @@ COMPANY_NAME = "Tesla Inc"
 
 if __name__ == '__main__':
     load_dotenv()
+    last_day = '2023-12-22'
+    day_before = '2023-12-21'
 
     # STEP 1: Use https://www.alphavantage.co
     # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -19,15 +21,30 @@ if __name__ == '__main__':
         "apikey": stock_apikey
     }
     stock_data = requests.get(url=stock_url, params=stock_url_parameters).json()
-    stock_last_day_close = float(stock_data['Time Series (Daily)']['2023-12-22']['4. close'])
-    stock_day_before_close = float(stock_data['Time Series (Daily)']['2023-12-21']['4. close'])
+    stock_last_day_close = float(stock_data['Time Series (Daily)'][last_day]['4. close'])
+    stock_day_before_close = float(stock_data['Time Series (Daily)'][day_before]['4. close'])
     stock_diff_percent = round(((stock_last_day_close - stock_day_before_close) / stock_last_day_close) * 100, 2)
-
-    if abs(stock_diff_percent) >= 5:
-        print("Get News")
 
     # STEP 2: Use https://newsapi.org
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    if abs(stock_diff_percent) >= 5:
+        news_apikey = os.getenv('NEWS_API_KEY')
+        news_url = 'https://newsapi.org/v2/everything'
+        news_url_parameters = {
+            "q": COMPANY_NAME,
+            "from": day_before,
+            "to": last_day,
+            "sortBy": "popularity",
+            "apiKey": news_apikey
+        }
+        news_data = requests.get(url=news_url, params=news_url_parameters).json()['articles'][:3]
+        articles = []
+        for article in news_data:
+            a = (f"Title: {article['title']}\n"
+                 f"Description: {article['description']}\n"
+                 f"Source: {article['source']['name']}")
+            articles.append(a)
+
 
     # STEP 3: Use https://www.twilio.com
     # Send a separate message with the percentage change and each article's title and description to your phone number.
