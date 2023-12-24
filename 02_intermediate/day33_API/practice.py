d26361220535cdb8e995003b4b63b569bedcd5b0
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-24 14:45:06 +0100

    Update day33 (API)

---
+++ 02_intermediate/day33_API/practice.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MY_LAT = os.getenv('MY_LAT')
+MY_LNG = os.getenv('MY_LNG')
+
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+# Bad practice:
+# if response.status_code == 404:
+#     raise Exception("Not found content!")
+# elif response.status_code != 200:
+#     raise Exception("Bad response from ISS API")
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+
+print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data['results']['sunrise'].split("T")[1].split("+")[0]
+sunset = data['results']['sunset'].split("T")[1].split("+")[0]
+
+print(data)
+print(sunrise[:5])
+print(sunset[:5])
