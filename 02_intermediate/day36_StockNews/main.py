5adc0f6dac4c1cad40eaa2cfd4fe52c0b34ea065
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-25 00:23:44 +0100

    Add STEP 1

--- 02_intermediate/day36_StockNews/main.py
+++ 02_intermediate/day36_StockNews/main.py
@@ -11,7 +11,20 @@ if __name__ == '__main__':
 
     # STEP 1: Use https://www.alphavantage.co
     # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-    
+    stock_apikey = os.getenv('STOCK_API_KEY')
+    stock_url = 'https://www.alphavantage.co/query'
+    stock_url_parameters = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": STOCK,
+        "apikey": stock_apikey
+    }
+    stock_data = requests.get(url=stock_url, params=stock_url_parameters).json()
+    stock_last_day_close = float(stock_data['Time Series (Daily)']['2023-12-22']['4. close'])
+    stock_day_before_close = float(stock_data['Time Series (Daily)']['2023-12-21']['4. close'])
+    stock_diff_percent = round(((stock_last_day_close - stock_day_before_close) / stock_last_day_close) * 100, 2)
+
+    if abs(stock_diff_percent) >= 5:
+        print("Get News")
 
     # STEP 2: Use https://newsapi.org
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
