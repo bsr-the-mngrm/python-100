6ab12e4fc1f3aa5e81d14aa306e39bf587bacc3c
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-24 14:39:42 +0100

    Update main.py

--- 02_intermediate/day35_Keys-Auth-EnvVars/main.py
+++ 02_intermediate/day35_Keys-Auth-EnvVars/main.py
@@ -1,3 +1,18 @@
+from dotenv import load_dotenv
+import os
+import requests
 
 if __name__ == '__main__':
-    pass
+    load_dotenv()
+    weather_api_key = os.getenv('WEATHER_API_KEY')
+    my_lat = os.getenv('MY_LAT')
+    my_lng = os.getenv('MY_LNG')
+    parameters = {
+        "appid": weather_api_key,
+        "lat": my_lat,
+        "lon": my_lng
+    }
+
+    data = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+
+    print(data.json())
