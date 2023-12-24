0602ec85f0e3d98da2251132f48e032e02de8815
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-24 11:00:32 +0100

    Add unescapeing HTML entities

--- 02_intermediate/day34_API_Practice/main.py
+++ 02_intermediate/day34_API_Practice/main.py
@@ -4,7 +4,7 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"].replace("&quot;", "\"")
+    question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
