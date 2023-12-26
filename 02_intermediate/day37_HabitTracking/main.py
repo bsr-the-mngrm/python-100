091f18ae5813efa462487c689bf67629f5db6ee4
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-26 22:12:47 +0100

    Setup user account on pixela

--- 02_intermediate/day37_HabitTracking/main.py
+++ 02_intermediate/day37_HabitTracking/main.py
@@ -5,3 +5,21 @@ from dotenv import load_dotenv
 
 if __name__ == '__main__':
     load_dotenv()
+
+    username = os.getenv('USER')
+    token = os.getenv('TOKEN')
+
+    print(username)
+
+    pixela_endpoint = "https://pixe.la/v1/users"
+
+    user_parameters = {
+        "token": token,
+        "username": username,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+    }
+
+    response = requests.post(url=pixela_endpoint, json=user_parameters)
+
+    print(response.text)
