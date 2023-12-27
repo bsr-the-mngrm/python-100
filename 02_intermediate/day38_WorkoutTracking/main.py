c4e5d6a7f50794e95d360710f70d23b0267ac25a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-27 22:56:38 +0100

    Initialization of day38 (Workout Tracking)

---
+++ 02_intermediate/day38_WorkoutTracking/main.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+APP_ID = os.getenv('APP_ID')
+API_KEY = os.getenv('API_KEY')
+
+if __name__ == '__main__':
+    pass
