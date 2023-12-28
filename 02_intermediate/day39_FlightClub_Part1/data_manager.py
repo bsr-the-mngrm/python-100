b9b0e76ae403acfad985fef1bc08c50b939bbf6b
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-28 18:06:10 +0100

    Define 'get_deals' function

--- 02_intermediate/day39_FlightClub_Part1/data_manager.py
+++ 02_intermediate/day39_FlightClub_Part1/data_manager.py
@@ -1,3 +1,31 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+from flight_data import FlightData
+from flight_search import FlightSearch
+
+
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.sheety_url = os.getenv('SHEETY_URL')
+        self.sheety_header = {
+            "Authorization": f"Basic {os.getenv('SHEETY_TOKEN')}",
+            "Username": os.getenv('USER'),
+            "Password": os.getenv('PASSWORD')
+        }
+        self.flight_search = FlightSearch()
+
+    def get_deals(self) -> list:
+        deals = requests.get(url=self.sheety_url, headers=self.sheety_header).json()['prices']
+
+        for deal in deals:
+            if deal['iataCode'] == '':
+                deal['iataCode'] = self.flight_search.get_iata_code(deal['city'])
+
+        return deals
+
+    def update_deals(self):
+        pass
--- 02_intermediate/day39_FlightClub_Part1/flight_data.py
+++ 02_intermediate/day39_FlightClub_Part1/flight_data.py
@@ -1,6 +1,7 @@
 class FlightData:
     # This class is responsible for structuring the flight data.
-    def __init__(self, fly_from, fly_to, date_from, date_to, price):
+    def __init__(self, flight_id, fly_from, fly_to, date_from, date_to, price):
+        self.id = flight_id
         self.fly_from = fly_from
         self.fly_to = fly_to
         self.price = price
