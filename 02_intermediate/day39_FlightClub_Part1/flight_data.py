f1af4c98de8ea3f338bc4bc1a8bccfe9e26c34af
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-28 17:24:34 +0100

    Update flight_data.py

--- 02_intermediate/day39_FlightClub_Part1/flight_data.py
+++ 02_intermediate/day39_FlightClub_Part1/flight_data.py
@@ -1,3 +1,8 @@
 class FlightData:
     # This class is responsible for structuring the flight data.
-    pass
+    def __init__(self, fly_from, fly_to, date_from, date_to, price):
+        self.fly_from = fly_from
+        self.fly_to = fly_to
+        self.price = price
+        self.date_from = date_from
+        self.date_to = date_to
