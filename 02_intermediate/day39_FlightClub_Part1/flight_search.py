be979468406f20204296502c7a73bd43bb4db982
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-28 17:59:47 +0100

    Define 'get_iata_code' function in FlightSearch class

--- 02_intermediate/day39_FlightClub_Part1/flight_search.py
+++ 02_intermediate/day39_FlightClub_Part1/flight_search.py
@@ -1,3 +1,24 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.kiwi_apikey = os.getenv('KIWI_APIKEY')
+        self.location_query_endpoint = "https://api.tequila.kiwi.com/locations/query"
+        self.kiwi_header = {
+            "apikey": self.kiwi_apikey,
+            "accept": "application/json"
+        }
+
+    def get_iata_code(self, city: str) -> str:
+        location_query = {
+            "term": city
+        }
+
+        response = requests.get(url=self.location_query_endpoint, params=location_query, headers=self.kiwi_header)
+
+        return response.json()['locations'][0]['code']
