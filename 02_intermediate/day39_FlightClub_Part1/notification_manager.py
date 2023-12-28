dab871c85d3b2ca1c2409bb1c3c5ccfc7c30386b
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-28 16:02:32 +0100

    Define Notification Manager class

--- 02_intermediate/day39_FlightClub_Part1/notification_manager.py
+++ 02_intermediate/day39_FlightClub_Part1/notification_manager.py
@@ -1,3 +1,22 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+        self.auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        self.from_phone_number = os.getenv('FROM_PHONE_NUMBER')
+        self.to_phone_number = os.getenv('TO_PHONE_NUMBER')
+
+    def send_sms(self, msg: str):
+        client = Client(self.account_sid, self.auth_token)
+        message = client.messages \
+            .create(
+                body=msg,
+                from_=self.from_phone_number,
+                to=self.to_phone_number
+            )
