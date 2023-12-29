381313fc5aee95b3c95dfef848f2e8378f4efb2a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-29 23:58:36 +0100

    Update data_manager.py

--- 02_intermediate/day40_FlightClub_Part2/data_manager.py
+++ 02_intermediate/day40_FlightClub_Part2/data_manager.py
@@ -9,12 +9,16 @@ class DataManager:
     def __init__(self):
         load_dotenv()
         self.sheety_url = os.getenv('SHEETY_URL')
+        self.sheety_user_url = os.getenv('SHEETY_USERS_URL')
         self.sheety_header = {
             "Authorization": f"Basic {os.getenv('SHEETY_TOKEN')}",
             "Username": os.getenv('USER'),
             "Password": os.getenv('PASSWORD')
         }
         self.flight_search = FlightSearch()
+
+    def get_customers(self):
+        return requests.get(url=self.sheety_user_url, headers=self.sheety_header).json()['users']
 
     def get_deals(self) -> list:
         deals = requests.get(url=self.sheety_url, headers=self.sheety_header).json()['prices']
