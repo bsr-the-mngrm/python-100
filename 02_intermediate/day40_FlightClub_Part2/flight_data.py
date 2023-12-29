58d91f571f9c8e87237443637dd85dcde10dc24b
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2023-12-29 23:41:11 +0100

    Update flight_data.py

--- 02_intermediate/day40_FlightClub_Part2/flight_data.py
+++ 02_intermediate/day40_FlightClub_Part2/flight_data.py
@@ -1,13 +1,13 @@
 class FlightData:
     # This class is responsible for structuring the flight data.
-    def __init__(self, fly_from, fly_to, out_date, return_date, price):
+    def __init__(self, fly_from, fly_to, out_date, return_date, price, stop_overs=0, via_city=""):
         self.fly_from = fly_from
         self.fly_to = fly_to
         self.price = price
         self.out_date = out_date
         self.return_date = return_date
-        self.stop_overs = 0
-        self.via_city = ""
+        self.stop_overs = stop_overs
+        self.via_city = via_city
 
     def message_text(self) -> str:
         return (f"Low price alert! Only {self.price} EUR  to fly from {self.fly_from} to {self.fly_to} "
