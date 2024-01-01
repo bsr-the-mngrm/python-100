9c27ddb106069c041f1b5b8af6686c2dcd617dd5
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-01 18:49:42 +0100

    Complete '100 best movies' project

--- 02_intermediate/day45_WebScraping/main.py
+++ 02_intermediate/day45_WebScraping/main.py
@@ -5,4 +5,16 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+if __name__ == '__main__':
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "html.parser")
 
+    title_elements = soup.find_all(name="h3", class_="title")
+    titles = [title.getText() for title in title_elements[::-1]]
+
+    # for i in range(len(title_elements)-1, -1, -1):
+    #     titles.append(title_elements[i].getText())
+
+    with open(file=".data/best-movies.txt", mode="w", encoding="utf-8") as text_file:
+        for title in titles:
+            text_file.write(title + '\n')
