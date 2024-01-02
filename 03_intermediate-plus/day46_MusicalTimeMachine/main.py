1e79782b89c8518c788ca8d788eff6f2a83b66a5
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-02 20:17:38 +0100

    Add 'get_song_titles' function (step1)

--- 03_intermediate-plus/day46_MusicalTimeMachine/main.py
+++ 03_intermediate-plus/day46_MusicalTimeMachine/main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from os import system
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
 BILLBOARD_URL = "https://www.billboard.com/charts/hot-100/"
 
@@ -14,8 +14,7 @@ def user_input() -> str:
     user_choice = None
     while user_choice != "":
         try:
-            user_choice = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: \n"
-                                "(do not write anything and hit 'Enter' to exit) \n")
+            user_choice = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: \n")
             date_format = '%Y-%m-%d'
             if user_choice != "":
                 date_obj = datetime.strptime(user_choice, date_format)
@@ -25,8 +24,23 @@ def user_input() -> str:
         else:
             return user_choice
 
-    return user_choice
+    print("\n We will go with default value: 2000-08-12")
+    return '2000-08-12'
+
+
+def get_song_titles(date: str) -> list:
+    """
+        Returns a list of sing titles
+        - Webscraping Billboard Hot100 on the given date
+    """
+
+    response = requests.get(f"{BILLBOARD_URL}/{date}")
+    billboard_soup = BeautifulSoup(response.text, 'html.parser')
+    song_titles = [str(song.getText()).strip() for song in billboard_soup.select("li ul li h3")]
+
+    return song_titles
 
 
 if __name__ == '__main__':
-    pass
+    selected_date = user_input()
+    song_title_list = get_song_titles(selected_date)
