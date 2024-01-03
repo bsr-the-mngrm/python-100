0f39fbf468c4402b212547fc37e01518c29d281a
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-03 14:58:52 +0100

    Add 'get_amazon_price'  function (step1)

--- 03_intermediate-plus/day47_AmazonPriceTracker/main.py
+++ 03_intermediate-plus/day47_AmazonPriceTracker/main.py
@@ -1,3 +1,23 @@
+import requests
+import smtplib
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+LOWER_LIMIT = 70
+
+
+def get_amazon_price(url: str) -> float:
+    """Returns the price of the product on the given URL"""
+    response = requests.get(url)
+    amazon_soup = BeautifulSoup(response.text, 'html.parser')
+    price = float(amazon_soup.find(class_="a-offscreen").getText()[1:])
+
+    return price
+
 
 if __name__ == '__main__':
-    pass
+    product_amazon_url = "https://www.amazon.com/Amazon-Basics-Turntable-Speakers-Bluetooth/dp/B0BFHTPLNP/"
+
+    product_amazon_price = get_amazon_price(product_amazon_url)
+
+    print(product_amazon_price)
