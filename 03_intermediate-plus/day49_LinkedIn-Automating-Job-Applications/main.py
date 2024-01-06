402ffc80aa6582162f43df1c57e0d9cdcb0df38e
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-06 17:36:57 +0100

    Add 'login' function (step1-2)

--- 03_intermediate-plus/day49_LinkedIn-Automating-Job-Applications/main.py
+++ 03_intermediate-plus/day49_LinkedIn-Automating-Job-Applications/main.py
@@ -1,3 +1,32 @@
+import os
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+load_dotenv()
+LINKEDIN_JOBS_URL = os.getenv('LINKEDIN_JOBS_URL')
+USERNAME = os.getenv('EMAIL_ADDRESS')
+PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+
+def login():
+    """Login to LinkedIn"""
+    username_input = driver.find_element(By.ID, 'session_key')
+    username_input.send_keys(USERNAME)
+
+    password_input = driver.find_element(By.ID, 'session_password')
+    password_input.send_keys(PASSWORD)
+    password_input.send_keys(Keys.ENTER)
+
 
 if __name__ == '__main__':
-    pass
+    # SELENIUM INITIALIZATION
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # LOGIN TO LINKEDIN
+    login()
+    driver.get(LINKEDIN_JOBS_URL)
