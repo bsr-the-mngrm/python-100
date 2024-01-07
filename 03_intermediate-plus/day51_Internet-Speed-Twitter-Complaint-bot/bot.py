3531bf2490db84ff3e0170ee3ee05e796aae9b46
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-07 19:44:37 +0100

    Define 'tweet_at_provider' function (step4)

--- 03_intermediate-plus/day51_Internet-Speed-Twitter-Complaint-bot/bot.py
+++ 03_intermediate-plus/day51_Internet-Speed-Twitter-Complaint-bot/bot.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
+
 
 class InternetSpeedTwitterBot:
     def __init__(self, up: int, down: int, isp_twitter_username: str):
@@ -12,7 +15,8 @@ class InternetSpeedTwitterBot:
         self.driver = webdriver.Chrome(options=chrome_options)
         self.isp_twitter_username = isp_twitter_username
 
-    def get_internet_speed(self):
+    def get_internet_speed(self) -> dict:
+        """Returns the internet speed (up and down values in Mbps)"""
         self.driver.get('https://www.speedtest.net')
         sleep(3)
         self.driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
@@ -23,5 +27,36 @@ class InternetSpeedTwitterBot:
 
         return {"up": up, "down": down}
 
-    def tweet_at_provider(self):
-        pass
+    def tweet_at_provider(self, email: str, username: str, password: str, tweet: str):
+        """Login to Twitter and send tweet to ISP"""
+        # LOGIN TO TWITTER
+        self.driver.get('https://twitter.com/i/flow/login')
+        sleep(3)
+        self.driver.find_element(By.NAME, 'text').send_keys(email, Keys.ENTER)
+        try:
+            sleep(3)
+            self.driver.find_element(By.NAME, 'password').send_keys(password, Keys.ENTER)
+
+        # It catches if the login service needs a username also
+        except NoSuchElementException:
+            sleep(3)
+            self.driver.find_element(By.NAME, 'text').send_keys(username, Keys.ENTER)
+            sleep(3)
+            self.driver.find_element(By.NAME, 'password').send_keys(password, Keys.ENTER)
+
+        # ACCEPT COOKIES
+        sleep(5)
+        accept_cookies_xpath = '//*[@id="layers"]/div/div/div/div/div/div[2]/div[1]/div'
+        self.driver.find_element(By.XPATH, accept_cookies_xpath).click()
+
+        # SEND TWEET TO ISP
+        whats_happening_xpath = ('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/'
+                                 'div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/'
+                                 'div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+
+        whats_happening = self.driver.find_element(By.XPATH, whats_happening_xpath)
+        whats_happening.send_keys(tweet)
+
+        post_xpath = ('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div'
+                      '/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div[3]/div')
+        self.driver.find_element(By.XPATH, post_xpath).click()
--- 03_intermediate-plus/day51_Internet-Speed-Twitter-Complaint-bot/main.py
+++ 03_intermediate-plus/day51_Internet-Speed-Twitter-Complaint-bot/main.py
@@ -6,11 +6,25 @@ load_dotenv()
 PROMISED_DOWN = 900
 PROMISED_UP = 300
 TWITTER_EMAIL = getenv('TWITTER_EMAIL')
+TWITTER_USERNAME = getenv('TWITTER_USERNAME')
 TWITTER_PASSWORD = getenv('TWITTER_PASSWORD')
 ISP_TWITTER_USERNAME = getenv('ISP_TWITTER_USERNAME')
 
 
 if __name__ == '__main__':
+    # INITIALIZE BOT
     my_bot = InternetSpeedTwitterBot(PROMISED_UP, PROMISED_DOWN, ISP_TWITTER_USERNAME)
-    print(my_bot.get_internet_speed())
-    my_bot.tweet_at_provider()
+
+    # GET INTERNET SPEED VALUES
+    result = my_bot.get_internet_speed()
+
+    # SEND A TWEET TO ISP IF RESULTS ARE LOWER THAN THE PROMISED ONES
+    if result['down'] < PROMISED_DOWN or result['up'] < PROMISED_UP:
+        tweet = (f"Dear @{ISP_TWITTER_USERNAME}, my internet speed is {result['down']}down/{result['up']}up.\n"
+                 f"(promised values: DOWN - {PROMISED_DOWN} Mbps / UP - {PROMISED_UP} Mbps)")
+        my_bot.tweet_at_provider(TWITTER_EMAIL, TWITTER_USERNAME, TWITTER_PASSWORD, tweet)
+        print("Tweet sent to ISP.")
+    else:
+        print(f"There is no complaints.\n"
+              f"My internet speed is {result['down']}down/{result['up']}up.\n"
+              f"(promised values: DOWN - {PROMISED_DOWN} Mbps / UP - {PROMISED_UP} Mbps)")
