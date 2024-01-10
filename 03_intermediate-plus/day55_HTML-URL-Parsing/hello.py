7c71a5313ee0c43e6188414b48a90b420b95a8e9
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-10 12:17:30 +0100

    Add 'rendering html elements' practice

--- 03_intermediate-plus/day55_HTML-URL-Parsing/hello.py
+++ 03_intermediate-plus/day55_HTML-URL-Parsing/hello.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ("<h1 style='text-align: center'>Hello, World!</h1>"
+            "<p>This is a paragraph</p>"
+            "<img src='https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzNibHhheHpqMGw1ZmNhNnl0M2xnNWpj"
+            "ZWJ5YzVmM3VrZzQyd3o1MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Cm9wKmKMUlRPvdoHgU/giphy.gif'>")
 
 
 # Different routes using the app.route decorator
