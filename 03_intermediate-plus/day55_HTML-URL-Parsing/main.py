7f1d89ddb5859e15f253a2537c3626a285db23a9
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-10 16:03:16 +0100

    Higher Lower Game - Add 'welcome_page' function

--- 03_intermediate-plus/day55_HTML-URL-Parsing/main.py
+++ 03_intermediate-plus/day55_HTML-URL-Parsing/main.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from random import randint
+
+app = Flask(__name__)
+RANDOM_NUMBER = randint(0, 9)
+
+
+# INDEX PAGE - WELCOME PAGE
+@app.route("/")
+def welcome_page():
+    return ("<h1>Guess a number between 0 and 9</h1>"
+            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>")
 
 
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)
