8959b10d89e35943d6cb7afe677ce416d19c93bb
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-10 12:36:24 +0100

    Add 'Python decorators to style HTML tags'

--- 03_intermediate-plus/day55_HTML-URL-Parsing/hello.py
+++ 03_intermediate-plus/day55_HTML-URL-Parsing/hello.py
@@ -11,10 +11,34 @@ def hello_world():
             "ZWJ5YzVmM3VrZzQyd3o1MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Cm9wKmKMUlRPvdoHgU/giphy.gif'>")
 
 
+def make_bold(func):
+    def wrapper():
+        return '<b>' + func() + '</b>'
+
+    return wrapper
+
+
+def make_emphasis(func):
+    def wrapper():
+        return '<em>' + func() + '</em>'
+
+    return wrapper
+
+
+def make_underlined(func):
+    def wrapper():
+        return '<u>' + func() + '</u>'
+
+    return wrapper
+
+
 # Different routes using the app.route decorator
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
-    return "<h1>Bye!</h1>"
+    return "Bye!"
 
 
 # Creating variable paths and converting the path to a specified data type
