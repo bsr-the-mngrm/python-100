5f3563afc69156a248abdc53e9736d5dec7b16b9
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-12 18:04:25 +0100

    Add jinja practice

--- 03_intermediate-plus/day57_Jinja-and-Flask/server.py
+++ 03_intermediate-plus/day57_Jinja-and-Flask/server.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from random import randint
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello world!"
+    random_number = randint(0, 9)
+    current_year = datetime.now().year
+    return render_template("index.html", num=random_number, current_year=current_year)
 
 
 if __name__ == '__main__':
