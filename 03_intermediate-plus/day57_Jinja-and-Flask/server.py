16c8e8f1f3576b43894f8d8ed4df73b812a77ce0
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-12 18:18:39 +0100

    Add 'guess' challenge (combining Jinja and APIs)

--- 03_intermediate-plus/day57_Jinja-and-Flask/server.py
+++ 03_intermediate-plus/day57_Jinja-and-Flask/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from random import randint
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,14 @@ def home():
     random_number = randint(0, 9)
     current_year = datetime.now().year
     return render_template("index.html", num=random_number, current_year=current_year)
+
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender = requests.get(f"https://api.genderize.io/?name={name}").json()['gender']
+    age = requests.get(f"https://api.agify.io/?name={name}").json()['age']
+
+    return render_template('guess.html', name=name, gender=gender, age=age)
 
 
 if __name__ == '__main__':
