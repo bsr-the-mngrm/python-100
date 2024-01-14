8c28811cb5e55d2fb4f3a7e83fed3dc6432239c5
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-14 15:33:56 +0100

    Add unique posts on index page

--- 04_advanced/day59_Blog-Project-Part2/main.py
+++ 04_advanced/day59_Blog-Project-Part2/main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
 import requests
 
-all_posts = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
+all_posts = requests.get('https://api.npoint.io/9e168a049320703f5f31').json()
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template("index.html", posts=all_posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 
 @app.route('/post/<post_id>')
