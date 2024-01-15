29f340d31c4648d3e31f58cbd78fc8449235c794
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-15 17:14:54 +0100

    Add email sending function
    With this method we can send the message to the host's email address and send a verification to the sender.

--- 04_advanced/day59_Blog-Project-Part2/main.py
+++ 04_advanced/day59_Blog-Project-Part2/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from os import getenv
 import requests
+import smtplib
+
 
 all_posts = requests.get('https://api.npoint.io/9e168a049320703f5f31').json()
 app = Flask(__name__)
@@ -18,6 +22,7 @@ def about():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
+        send_message(request.form)
         return render_template("contact.html", msg_sent=True)
     else:
         return render_template("contact.html", msg_sent=False)
@@ -26,6 +31,35 @@ def contact():
 @app.route('/post/<post_id>')
 def post_page(post_id):
     return render_template('post.html', post=all_posts[int(post_id)-1])
+
+
+def send_message(form: dict):
+    load_dotenv()
+    host_email_addr = getenv('EMAIL_ADDRESS')
+    host_email_addr_pass = getenv('EMAIL_APP_PASS')
+    smtp_addr = getenv('SMTP_ADDRESS')
+    smtp_port = getenv('SMTP_PORT')
+
+    with smtplib.SMTP_SSL(host=smtp_addr, port=smtp_port) as smtp_connection:
+        smtp_connection.login(user=host_email_addr, password=host_email_addr_pass)
+        # SEND MESSAGE TO HOST
+        smtp_connection.sendmail(
+            from_addr=host_email_addr,
+            to_addrs=host_email_addr,
+            msg=f"Subject:Message from {form['name']}\n\n"
+                f"Contact info:\n"
+                f"{form['name']} | {form['email']} | {form['phone']}\n"
+                f"Message:\n"
+                f"{form['message']}"
+        )
+
+        # SEND VERIFICATION TO SENDER
+        smtp_connection.sendmail(
+            from_addr=host_email_addr,
+            to_addrs=form['email'],
+            msg="Subject: Successfully sent message\n\n"
+                "We received your message. Thank you!"
+        )
 
 
 if __name__ == "__main__":
