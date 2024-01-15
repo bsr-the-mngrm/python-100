4243114169c16f9d5335c8d8b6a81fa1857d86ad
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-15 16:07:16 +0100

    Add HTTP method handling

--- 04_advanced/day60_HTML-Forms/main.py
+++ 04_advanced/day60_HTML-Forms/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def receive_data():
+    if request.method == 'POST':
+        return render_template('welcome.html', username=request.form['username'], password=request.form['password'])
+    else:
+        return home()
 
 
 if __name__ == '__main__':
