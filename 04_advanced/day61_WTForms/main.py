0e10e8f913b27056f7e06788efde4e3f9cd5eef1
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-16 20:28:20 +0100

    Add 'LoginForm' class

--- 04_advanced/day61_WTForms/main.py
+++ 04_advanced/day61_WTForms/main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 
 
 app = Flask(__name__)
+app.secret_key = "foobar"
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = PasswordField('Password')
 
 
 @app.route("/")
@@ -11,7 +19,8 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
