31ab1e14c4415b29d666b631d09a6d70f60421cb
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-16 20:33:53 +0100

    Add 'code improvements'

--- 04_advanced/day61_WTForms/main.py
+++ 04_advanced/day61_WTForms/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 
 
 app = Flask(__name__)
@@ -8,8 +8,9 @@ app.secret_key = "foobar"
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
+    email = StringField(label='Email: ')
+    password = PasswordField(label='Password: ')
+    submit = SubmitField(label="Log In")
 
 
 @app.route("/")
