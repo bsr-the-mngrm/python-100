e05041d74933d9c48ae5a7a5dfaf54a1668cc876
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-17 11:26:26 +0100

    Add received data handling

--- 04_advanced/day61_WTForms/main.py
+++ 04_advanced/day61_WTForms/main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from dotenv import load_dotenv
+from os import getenv
 
-
+load_dotenv()
 app = Flask(__name__)
-app.secret_key = "foobar"
+app.secret_key = getenv('FLASK_APP_SECRET_KEY')
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email: ')
-    password = PasswordField(label='Password: ')
+    email = StringField(label='Email: ', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password: ', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
@@ -18,10 +21,25 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == getenv('ADMIN_EMAIL') and login_form.password.data == getenv('ADMIN_PASSWORD'):
+            return redirect('/success')
+        else:
+            return redirect('/denied')
+    return render_template('login.html', form=login_form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
+@app.route('/denied')
+def denied():
+    return render_template('denied.html')
 
 
 if __name__ == '__main__':
