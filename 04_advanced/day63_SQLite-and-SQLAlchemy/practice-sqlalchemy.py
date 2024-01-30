3d6bcf70cb185b2e5ee4d1c9ae1e2bec738a36d3
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-30 18:27:35 +0100

    Practice CRUD operations with SQLAlchemy

--- 04_advanced/day63_SQLite-and-SQLAlchemy/practice-sqlalchemy.py
+++ 04_advanced/day63_SQLite-and-SQLAlchemy/practice-sqlalchemy.py
@@ -40,6 +40,44 @@ with app.app_context():
 
 # Create record
 with app.app_context():
-    new_book = Book(id=2, title='Harry Potter2', author='J.K. Rowling', rating=9.3)
+    # new_book = Book(id=2, title='Harry Potter2', author='J.K. Rowling', rating=9.3)
+    new_book = Book(title='Harry Potter3', author='J.K. Rowling', rating=9.3)
     db.session.add(new_book)
+    db.session.commit()
+
+
+# Read all records
+with app.app_context():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+    print(all_books.all())
+
+
+# Read a particular record by query
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+    print(book)
+
+
+# Update a particular record by query
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter2")).scalar()
+    book_to_update.title = "Harry Potter and the Chamber of Secrets"
+    db.session.commit()
+
+
+# Update a record by primary key
+book_id = 3
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_update = db.get_or_404(Book, book_id)
+    book_to_update.title = "Harry Potter and the Goblet of Fire"
+    db.session.commit()
+
+# Delete a particular record by primary key
+book_id = 1
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
     db.session.commit()
