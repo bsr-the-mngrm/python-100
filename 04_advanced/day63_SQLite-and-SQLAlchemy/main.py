7c5eb8ff26d2f905391dd2939e7507f0fa812d7f
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-30 19:16:26 +0100

    Add database initialization to the 'My Library' project

--- 04_advanced/day63_SQLite-and-SQLAlchemy/main.py
+++ 04_advanced/day63_SQLite-and-SQLAlchemy/main.py
@@ -1,12 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
+# CREATE FLASK APP
 app = Flask(__name__)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 all_books = []
 
 
+# CREATE TABLE
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
 @app.route('/')
 def home():
+    # Create table schema in the database
+    with app.app_context():
+        db.create_all()
     return render_template('index.html', books=all_books)
 
 
