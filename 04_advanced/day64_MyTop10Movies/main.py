4012d0122f91fc9246dd2ada1033216a66945c91
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-01-31 22:09:53 +0100

    Fix main.py

--- 04_advanced/day64_MyTop10Movies/main.py
+++ 04_advanced/day64_MyTop10Movies/main.py
@@ -8,7 +8,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from dotenv import load_dotenv
 from os import getenv
-import requests
 
 load_dotenv()
 
@@ -54,36 +53,6 @@ class RateMovieForm(FlaskForm):
     submit = SubmitField('Done')
 
 
-# # After adding the new_movie the code needs to be commented out/deleted.
-# # So you are not trying to add the same movie twice. The db will reject non-unique movie titles.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
-#                 "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads "
-#                 "to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family "
-#                 "(Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep "
-#                 "each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.add(second_movie)
-#     db.session.commit()
-
-
 @app.route("/")
 def home():
     my_top_movies = db.session.execute(db.select(Movie).order_by(Movie.ranking)).scalars().all()
@@ -99,16 +68,17 @@ def add():
 @app.route("/update", methods=['GET', 'POST'])
 def update():
     movie_id = request.args.get('id')
-    movie = db.get_or_404(Movie, movie_id)
+    movie_to_update = db.get_or_404(Movie, movie_id)
     form = RateMovieForm()
 
     if form.validate_on_submit():
-        movie.rating = float(form.rating.data)
-        movie.review = form.review.data
+        movie_to_update.rating = float(form.rating.data)
+        movie_to_update.review = form.review.data
         db.session.commit()
+
         return redirect(url_for('home'))
 
-    return render_template('edit.html', form=form, movie=movie)
+    return render_template('edit.html', form=form, movie=movie_to_update)
 
 
 @app.route("/delete")
@@ -117,4 +87,37 @@ def delete():
 
 
 if __name__ == '__main__':
+    # # After adding the new_movie the code needs to be commented out/deleted.
+    # # So you are not trying to add the same movie twice. The db will reject non-unique movie titles.
+    # new_movie = Movie(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an "
+    #                 "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's "
+    #                 "negotiation with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    # )
+    # second_movie = Movie(
+    #     title="Avatar The Way of Water",
+    #     year=2022,
+    #     description="Set more than a decade after the events of the first film, learn the story of the Sully family "
+    #                 "(Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep "
+    #                 "each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+    #     rating=7.3,
+    #     ranking=9,
+    #     review="I liked the water.",
+    #     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+    # )
+    #
+    # # CREATE TABLE SCHEMA IN THE DATABASE
+    # with app.app_context():
+    #     db.create_all()
+    #     db.session.add_all([new_movie, second_movie])
+    #     db.session.commit()
+    #
+    # app.run()
+
     app.run(debug=True)
