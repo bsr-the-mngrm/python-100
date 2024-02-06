020d1975f5906f9074f269e20621896e18bd7a4b
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-02-06 19:44:21 +0100

    Add 'Protect Routes'

--- 04_advanced/day69_Blog-Project-Part4/main.py
+++ 04_advanced/day69_Blog-Project-Part4/main.py
@@ -3,7 +3,7 @@ from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_login import UserMixin, AnonymousUserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.exc import IntegrityError
@@ -135,8 +135,19 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post, current_user=current_user)
 
 
+def admin_only(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not isinstance(current_user, AnonymousUserMixin) and current_user.id == 1:
+            return func(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
+
+
 # TODO: Use a decorator so only an admin user can create a new post
 @app.route("/new-post", methods=["GET", "POST"])
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -156,6 +167,7 @@ def add_new_post():
 
 # TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -178,6 +190,7 @@ def edit_post(post_id):
 
 # TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
