c486a3b0ff62a0d9d08ee7566c6bda8758cf7f77
bsr-the-mngrm <112482354+bsr-the-mngrm@users.noreply.github.com>
2024-02-06 20:16:19 +0100

    Add Comment Form

--- 04_advanced/day69_Blog-Project-Part4/main.py
+++ 04_advanced/day69_Blog-Project-Part4/main.py
@@ -12,7 +12,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 from os import getenv
-from forms import CreatePostForm, RegisterForm, LoginForm
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 load_dotenv()
 
@@ -138,7 +138,9 @@ def get_all_posts():
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
-    return render_template("post.html", post=requested_post, current_user=current_user)
+    comment_form = CommentForm()
+
+    return render_template("post.html", post=requested_post, current_user=current_user, form=comment_form)
 
 
 def admin_only(func):
